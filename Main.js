9c0f063e880d09a70830c99d8ffdc180eabdda2e
Afhaam1 <84372124+Afhaam1@users.noreply.github.com>
2022-05-29 21:02:44 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,18 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 500);
+    canvas.position(550,150),
+    poseNet = ml5.poseNet(video, modelLoaded) ;
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log('poseNet is Initialized');
+} 
+function gotPoses(results){
+  if(results.length > 0){
+      console.log(results)
+  }
+}function draw(){
+    background( 'ligtblue' )
+}
